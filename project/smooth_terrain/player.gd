e82883ba8099196da77000981f3496b0362200cc
Sean Dohring <seandohring@gmail.com>
2025-11-03 09:16:47 -0800

    mouse camera

--- project/smooth_terrain/player.gd
+++ project/smooth_terrain/player.gd
@@ -4,7 +4,25 @@ extends CharacterBody3D
 @export var jump_force = 1000.0
 @export var gravity = 18.0
 
+@onready var head_camera = %head_camera
+@onready var rear_camera = %rear_camera
+@onready var head_camera_container = %head_camera_container
+@onready var rear_camera_container = %rear_camera_container
+
+@export var mouse_sensitivity = 0.002
+
+func _ready() -> void:
+  Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+  head_camera.make_current()
+
+func _unhandled_input(event: InputEvent) -> void:
+  if not event is InputEventMouseMotion:
+    return
+  self.rotate_y(-event.relative.x * mouse_sensitivity)
+  head_camera_container.rotate_x(-event.relative.y * mouse_sensitivity)
+
 func _physics_process(delta):
+  rear_camera_container.global_transform = head_camera_container.global_transform
   var head = self
 
   var grounded = self.is_on_floor()
