6a16362d7af1cff96600f9d918df08326efe1196
Sean Dohring <seandohring@gmail.com>
2025-11-03 09:31:55 -0800

    digging and project key binds

--- project/smooth_terrain/player.gd
+++ project/smooth_terrain/player.gd
@@ -10,21 +10,26 @@ extends CharacterBody3D
 @onready var rear_camera_container = %rear_camera_container
 
 @export var mouse_sensitivity = 0.002
-
-var terrain: VoxelTerrain
+@export var dig_reach = 4.0
+@export var dig_radius = 1.5
 
 func _ready() -> void:
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
   head_camera.make_current()
-  terrain = Global.get_terrain()
 
 func _unhandled_input(event: InputEvent) -> void:
-  if not event is InputEventMouseMotion:
+  if not event is InputEventMouseMotion or Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
     return
   self.rotate_y(-event.relative.x * mouse_sensitivity)
   head_camera_container.rotate_x(-event.relative.y * mouse_sensitivity)
 
 func _physics_process(delta):
+  if Input.is_action_just_pressed("pause"):
+    if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+      Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    else:
+      Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
   rear_camera_container.global_transform = head_camera_container.global_transform
   var head = self
 
@@ -41,19 +46,34 @@ func _physics_process(delta):
   var right = head.transform.basis.x
   var up = Vector3(0, 1, 0)
   
-  if Input.is_key_pressed(KEY_A):
+  if Input.is_action_pressed("move_left"):
     self.velocity -= right * speed * delta
-  elif Input.is_key_pressed(KEY_D):
+  elif Input.is_action_pressed("move_right"):
     self.velocity += right * speed * delta
 
-  if Input.is_key_pressed(KEY_W):
+  if Input.is_action_pressed("move_forward"):
     self.velocity += forward * speed * delta
-  elif Input.is_key_pressed(KEY_S):
+  elif Input.is_action_pressed("move_backward"):
     self.velocity -= forward * speed * delta
-  
-  if grounded and Input.is_key_pressed(KEY_SPACE):
+
+  if grounded and Input.is_action_pressed("jump"):
     self.velocity += up * jump_force * delta
 
   self.velocity -= up * gravity * delta
 
   move_and_slide()
+
+  if Input.is_action_just_pressed("dig"):
+    dig(dig_radius)
+
+
+func dig(radius: float):
+  var vt := Global.get_terrain().get_voxel_tool()
+  vt.mode = VoxelTool.MODE_REMOVE
+  var origin = head_camera.global_transform.origin
+  var forward = -head_camera.global_transform.basis.z
+  var point = origin + forward * dig_reach
+  var diff = (origin - point).normalized()
+  for i in range(dig_reach):
+    var dig_point = origin - (i * diff)
+    vt.do_sphere(dig_point, radius)
