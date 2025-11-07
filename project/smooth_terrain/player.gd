5b408ce0515b29460eca9553fa9fa9d1304fd0b9
Sean Dohring <seandohring@gmail.com>
2025-11-07 11:25:25 -0800

    1p movement

--- project/smooth_terrain/player.gd
+++ project/smooth_terrain/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 @export var base_speed = 40.0
-@export var jump_force = 1000.0
+@export var jump_force = 10.0
 @export var gravity = 18.0
 
 @export var mouse_sensitivity = 0.002
@@ -13,6 +13,8 @@ extends CharacterBody3D
 @onready var label: Label3D = %label
 
 var id: int
+var camera_container: Node3D = null
+var camera: Camera3D = null
 var is_authority: bool = false
 
 func _enter_tree():
@@ -31,10 +33,24 @@ func _ready():
 
   label.text = self.name
 
+func _unhandled_input(event: InputEvent) -> void:
+  if not event is InputEventMouseMotion:
+    return
+  if not is_authority:
+    return
+  if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+    return
+  rotate_y(-event.relative.x * mouse_sensitivity)
+  if camera_container:
+    camera_container.rotate_x(-event.relative.y * mouse_sensitivity)
+
 func _set_up_camera():
-  var camera = Camera3D.new()
-  add_child(camera)
-  camera.position = Vector3(0, 2, -8)
+  camera_container = Node3D.new()
+  camera_container.position = Vector3(0, 2, 0)
+  add_child(camera_container)
+  camera = Camera3D.new()
+  camera_container.add_child(camera)
+  camera.position = Vector3(0, 0, 8)
   camera.look_at(Vector3(0, 3, 0), Vector3.UP)
   camera.make_current()
 
@@ -47,6 +63,30 @@ func _physics_process(delta):
   var up = Vector3(0, 1, 0)
   self.velocity -= up * gravity * delta
 
-  self.rotate_y(delta * 0.2)
+  if is_authority:
+    _handle_input(delta)
 
   move_and_slide()
+
+func _handle_input(delta: float):
+  var speed = base_speed
+  if not self.is_on_floor():
+    speed *= 0.5
+  if Input.is_action_pressed("move_forward"):
+    self.velocity += -transform.basis.z * speed * delta
+  elif Input.is_action_pressed("move_backward"):
+    self.velocity += transform.basis.z * speed * delta
+  
+  if Input.is_action_pressed("move_left"):
+    self.velocity += -transform.basis.x * speed * delta
+  elif Input.is_action_pressed("move_right"):
+    self.velocity += transform.basis.x * speed * delta
+  
+  if self.is_on_floor() and Input.is_action_just_pressed("jump"):
+    self.velocity.y = jump_force
+  
+  if Input.is_action_just_pressed("pause"):
+    if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+      Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    else:
+      Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
