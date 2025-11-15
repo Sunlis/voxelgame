144fd86ce2138e25cd383aeecd347ece150619ea
Sean Dohring <seandohring@gmail.com>
2025-11-14 20:32:36 -0800

    slightly better menu styling

--- project/smooth_terrain/build_menu_item.gd
+++ project/smooth_terrain/build_menu_item.gd
@@ -13,9 +13,14 @@ const ICONS = {
   set(v):
     build_type = v
     _update()
+@export var hovered: bool = false:
+  set(v):
+    hovered = v
+    _update()
 
 @onready var texture: TextureRect = %texture
 @onready var label: Label = %label
+@onready var color_rect: ColorRect = %color
 
 func _ready():
   _update()
@@ -25,3 +30,8 @@ func _update():
     return
   texture.texture = ICONS.get(build_type, null)
   label.text = BuildType.NAMES.get(build_type, "Unknown")
+  color_rect.visible = hovered
+  if hovered:
+    texture.modulate = Color(1, 1, 1, 1)
+  else:
+    texture.modulate = Color(0.5, 0.5, 0.5, 1)
