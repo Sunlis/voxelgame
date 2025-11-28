0c6d24ff89d248b8a5515e90792d092a6b8c2a31
Sean Dohring <seandohring@gmail.com>
2025-11-28 12:12:33 -0800

    trying (and failing) to freeze drops in terrain

--- project/smooth_terrain/player.gd
+++ project/smooth_terrain/player.gd
@@ -297,6 +297,6 @@ func dig(origin: Vector3, direction: Vector3, radius: float):
   for i in Util.rangef(0.0, dig_reach, radius / 2.0):
     var dig_point = origin - (i * diff)
     vt.do_sphere(dig_point, radius)
-    Global.terrain_modified.emit(dig_point, radius * 2.0)
+    Global.terrain_modified.emit(dig_point, radius)
     if TerrainUtil.sphere_intersect(dig_point, radius):
       Global.create_drop(dig_point, drop_rate)
--- project/smooth_terrain/terrain.gd
+++ project/smooth_terrain/terrain.gd
@@ -1,5 +1,7 @@
 extends VoxelTerrain
 
+const DropScene = preload("res://drop.tscn")
+
 @export var noises: Array[FastNoiseLite] = []
 
 func _ready() -> void:
@@ -16,7 +18,11 @@ func _sample_noise(pos: Vector3) -> float:
 
 var _marked = {}
 
+var _drops = []
+
 func _on_terrain_modified(pos: Vector3, radius: float) -> void:
+  if len(_drops) > 0:
+    return
   radius *= 2.0
   for x in range(floor(pos.x - radius), ceil(pos.x + radius) + 1):
     for y in range(floor(pos.y - radius), ceil(pos.y + radius) + 1):
@@ -24,43 +30,18 @@ func _on_terrain_modified(pos: Vector3, radius: float) -> void:
         var sample_pos = Vector3(x, y, z)
         if sample_pos in _marked:
           continue
+        if sample_pos.y > 0:
+          continue
         _marked[sample_pos] = true
         var sample = _sample_noise(sample_pos)
         if sample > 0.64:
+          # print('create drop at %s' % sample_pos)
           _create_drop(sample_pos)
-        # var sphere = CSGSphere3D.new()
-        # sphere.radius = 0.1
-        # add_child(sphere)
-        # sphere.global_position = sample_pos
-        # sphere.material = StandardMaterial3D.new()
-        # sphere.material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
-        # var c = Color(0, sample, 0)
-        # if sample > 0.65:
-        #   c.r = 1.0
-        # elif sample > 0.5:
-        #   c.b = 1.0
-        # sphere.material.albedo_color = c
-        # sphere.material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+          return
 
 func _create_drop(pos: Vector3):
-  var drop = RigidBody3D.new()
-  # drop.sleeping = true
+  var drop = DropScene.instantiate() as Drop
+  drop.freeze_in_wall = true
   add_child(drop)
-  drop.global_transform.origin = pos
-  drop.contact_monitor = true
-  drop.max_contacts_reported = 2
-  drop.set_collision_layer_value(1, false)
-  drop.set_collision_mask_value(1, true)
-  drop.set_collision_layer_value(5, true) # Terrain
-  drop.set_collision_layer_value(10, true) # Props
-  drop.set_collision_mask_value(10, true) # Props
-  drop.set_collision_layer_value(15, true) # Drops
-  drop.set_collision_mask_value(15, true) # Drops
-  drop.body_exited.connect(func(_b):
-    drop.sleeping = false)
-
-  var mesh_instance = MeshInstance3D.new()
-  drop.add_child(mesh_instance)
-  mesh_instance.mesh = SphereMesh.new()
-  mesh_instance.mesh.radius = 0.1
-  mesh_instance.mesh.height = 0.2
+  drop.global_position = pos
+  _drops.append(drop)
