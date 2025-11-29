e9624e112ff708f8ec99ce3cca6db6eb3199cb65
Sean Dohring <seandohring@gmail.com>
2025-11-29 10:41:19 -0800

    Revert "clean up drop experiment"
    This reverts commit d808e942a8981ac48ee165e698ead418e69940d4.

--- project/smooth_terrain/terrain.gd
+++ project/smooth_terrain/terrain.gd
@@ -2,7 +2,11 @@
 
 extends VoxelTerrain
 
-@export var noises: Array[ZN_FastNoiseLite] = []
+const DropScene = preload("res://drop.tscn")
+
+@export var cave_noises: Array[FastNoiseLite] = []
+
+@export var drop_noises: Array[ZN_FastNoiseLite] = []
 
 const CavesGeneratorScript = preload("res://caves_generator.gd")
 
@@ -16,11 +20,13 @@ const DENSITY = 9
 var _marks: Array[Label3D] = []
 
 func _ready() -> void:
-  for noise in noises:
+  for noise in cave_noises:
     noise.seed = randi()
   self.generator = CavesGeneratorScript.new()
-  self.generator.noises = noises
+  self.generator.noises = cave_noises
   self.run_stream_in_editor = true
+  for noise in drop_noises:
+    noise.seed = randi()
   Global.register_terrain(self)
   if DRAW_DEBUG_LABELS:
     for x in range(int(check_area.position.x), int(check_area.position.x + check_area.size.x), DENSITY):
@@ -42,3 +48,40 @@ func _process(_delta):
   for mark in _marks:
     mark.text = "%.2f (%d)" % [vt.get_voxel_f(mark.global_position), vt.get_voxel(mark.global_position)]
 
+  Global.terrain_modified.connect(_on_terrain_modified)
+
+func _sample_noise(pos: Vector3) -> float:
+  var out = 0.0
+  for noise in drop_noises:
+    out += (noise.get_noise_3dv(pos) + 1.0) / 2.0
+  return out / float(drop_noises.size())
+
+var _marked = {}
+
+var _drops = []
+
+func _on_terrain_modified(pos: Vector3, radius: float) -> void:
+  if len(_drops) > 0:
+    return
+  radius *= 2.0
+  for x in range(floor(pos.x - radius), ceil(pos.x + radius) + 1):
+    for y in range(floor(pos.y - radius), ceil(pos.y + radius) + 1):
+      for z in range(floor(pos.z - radius), ceil(pos.z + radius) + 1):
+        var sample_pos = Vector3(x, y, z)
+        if sample_pos in _marked:
+          continue
+        if sample_pos.y > 0:
+          continue
+        _marked[sample_pos] = true
+        var sample = _sample_noise(sample_pos)
+        if sample > 0.64:
+          # print('create drop at %s' % sample_pos)
+          _create_drop(sample_pos)
+          return
+
+func _create_drop(pos: Vector3):
+  var drop = DropScene.instantiate() as Drop
+  drop.freeze_in_wall = true
+  add_child(drop)
+  drop.global_position = pos
+  _drops.append(drop)
