a223886bd6b38e293bd4e4cfe412b82989aa3818
Sean Dohring <seandohring@gmail.com>
2026-01-07 22:05:15 -0800

    start scaling everything up
    instead of scaling terrain down, just make everything bigger

--- project/player_config.gd
+++ project/player_config.gd
@@ -7,12 +7,12 @@ class_name PlayerConfig
 @export var gravity = 18.0
 
 @export var mouse_sensitivity = 0.002
-@export var dig_reach = 4.0
-@export var pickup_reach = 8.0
-@export var dig_radius = 1.0
+@export var dig_reach = 16.0
+@export var pickup_reach = 32.0
+@export var dig_radius = 4.0
 @export var dig_noise: float = 0.02
 
 @export var drop_rate: float = 0.05
 
-@export var build_reach = 12.0
+@export var build_reach = 48.0
 @export var build_rotate_speed = 5.0
